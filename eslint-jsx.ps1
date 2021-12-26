833d73a2cf52e3bbc283c8441026b8771fab944c
hrmrafsan <59786405+RafsanAmin@users.noreply.github.com>
2021-12-26 14:38:46 +0600

    Add files via upload

---
+++ eslint-jsx.ps1
@@ -0,0 +1,5 @@
+git clone 'https://github.com/RafsanAmin/eslint-configure.git'
+cd './eslint-configure'
+mv 'a.eslintrc' '.eslintrc'
+copy * 'C:\Users\RAFSAN-PC\AppData\Roaming\npm\'
+rmdir './eslint-configure'
